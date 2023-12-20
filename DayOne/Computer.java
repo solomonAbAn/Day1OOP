1f5a31ce5bf081a89783df393ad4050b2aa21b92
Sleiman Abou antoun <sleimen.aa@gmail.com>
2023-12-20 16:18:49 -0500

    first commit

---
+++ DayOne/Computer.java
@@ -0,0 +1,38 @@
+package DayOne;
+
+
+public class Computer {
+
+	private 
+	//brand(String)
+	String brand;
+	//SN(long)
+	long SN;
+	//price(double)
+	double price;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ DayOne/assignmentInClass.java
@@ -0,0 +1,25 @@
+package DayOne;
+
+public class assignmentInClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//computer object
+		//four attributes
+		//brand(String)
+		//SN(long)
+		//price(double)
+		
+		
+		//initialize with valid values
+		
+		//enough flexibility 
+ 		
+	
+		
+		
+		
+		
+	}
+
+}
